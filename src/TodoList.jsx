c617b0ca6f568796daab48e04d9add0d3a981165
OsirisJEBJ <88217230+OsirisJEBJ@users.noreply.github.com>
2026-04-27 11:25:58 -0700

    Fix indentation and formatting in TodoList component

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,11 +1,12 @@
 import TodoListItem from './TodoListItem'; 
-function TodoList({todoList}){
-  
-    return(
- <ul>
-    {todoList.map(todo =>( 
-      <TodoListItem key={todo.id} todo={todo} />
-    ))}
-  </ul>    );
+
+function TodoList({todoList}) {
+    return (
+      <ul>
+        {todoList.map((todo) => ( 
+          <TodoListItem key={todo.id} todo={todo} />
+        ))}
+     </ul>   
+    );
 }
 export default TodoList;
