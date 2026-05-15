6bc843d2e7a8a8e7b12c61fad3b89f4006063699
OsirisJEBJ <estradasanchezjao@gmail.com>
2026-05-15 13:57:59 -0700

    test

--- src/features/TodoForm.jsx
+++ src/features/TodoForm.jsx
@@ -1,10 +1,8 @@
-import {useRef} from 'react';
 import { useState } from 'react';
 import TextInputWithLabel from '../shared/TextInputWithLabel';
 import {isValidTodoTitle} from '../utils/todoValidation';
 function TodoForm({onAddTodo}){
   const [workingTodoTitle, setWorkingTodoTitle] = useState('');
-  const inputRef = useRef();
   const handleAddTodo = (event) => {
     event.preventDefault();
     onAddTodo(workingTodoTitle);
@@ -19,7 +17,6 @@ function TodoForm({onAddTodo}){
       <form onSubmit={handleAddTodo}>
       
         <TextInputWithLabel
-          inputref={inputRef}
           value={workingTodoTitle}
           onChange={(e) => setWorkingTodoTitle(e.target.value)}
           elementId={'todoTitle'}
--- src/features/TodoList/TodoList.jsx
+++ src/features/TodoList/TodoList.jsx
@@ -3,7 +3,7 @@ function TodoList({todoList, onCompleteTodo, onUpdateTodo}) {
   const filteredTodoList = todoList.filter(todo => !todo.isCompleted);
   
     return(
-    filteredTodoList.length === 0 ?( <p>Add todo above to get started!</p>) : (
+    filteredTodoList.length === 0 ?( <p>Add todo above to get started</p>) : (
  <ul>
     {filteredTodoList.map(todo =>( 
       <TodoListItem  onCompleteTodo={onCompleteTodo} key={todo.id} todo={todo} onUpdateTodo={onUpdateTodo} />
